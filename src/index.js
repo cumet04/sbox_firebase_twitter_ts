5858d1121dd336e9a2c7f5012ea9c2b65b154f3c
medalhkr <medalhkr@gmail.com>
2019-02-03 19:47:08 +0900

    using mock

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-var Twitter = require("twitter");
+const use_twitter = false
+const fs = require("fs")
 
-var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_API_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_API_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
+let client
+if (use_twitter) {
+  const Twitter = require("twitter")
+  client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_API_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_API_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  })
+}
 
-var params = { count: 5 };
-client.get("statuses/home_timeline.json", params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
+function home_timeline() {
+  if (use_twitter) {
+    return client.get("statuses/home_timeline.json", { count: 10 })
+  } else {
+    return JSON.parse(fs.readFileSync("mock/statuses/home_timeline.json", "utf8"))
   }
-});
+}
+async function main() {
+  console.log(JSON.stringify(await home_timeline()))
+}
+
+main()
