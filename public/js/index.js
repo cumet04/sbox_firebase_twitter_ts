0628f9e699cc289970a2fb11a36d6f2b1971ec17
medalhkr <medalhkr@gmail.com>
2019-02-11 21:52:26 +0900

    twitter login

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-  // // The Firebase SDK is initialized and available here!
-  //
-  // firebase.auth().onAuthStateChanged(user => { });
-  // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
-  // firebase.messaging().requestPermission().then(() => { });
-  // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
-  //
-  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-
   try {
     let app = firebase.app()
     let features = ["auth", "database", "messaging", "storage"].filter(
@@ -18,8 +8,39 @@ document.addEventListener("DOMContentLoaded", function() {
   } catch (e) {
     console.error(e)
     document.getElementById("load").innerHTML = "Error loading the Firebase SDK, check the console."
+    return
   }
 
+  var provider = new firebase.auth.TwitterAuthProvider()
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log(`login: ${user.displayName}`)
+      return
+    }
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function(result) {
+        // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+        // You can use these server side with your app's credentials to access the Twitter API.
+        var token = result.credential.accessToken
+        var secret = result.credential.secret
+        // The signed-in user info.
+        var user = result.user
+        // ...
+        console.log(user)
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code
+        var errorMessage = error.message
+        // The email of the user's account used.
+        var email = error.email
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential
+        // ...
+      })
+  })
   fetch("/api/a")
     .then(res => {
       return res.json()
