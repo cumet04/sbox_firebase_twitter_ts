a35a61c215147af8d225eb11cf8ba23f039da210
medalhkr <medalhkr@gmail.com>
2019-02-11 17:17:41 +0900

    mount functions on hosting domain

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,50 +1,48 @@
-import * as express from "express";
-import * as functions from "firebase-functions";
-import * as fs from "fs";
+import * as express from "express"
+import * as functions from "firebase-functions"
+import * as fs from "fs"
 
-const use_twitter = false;
+const use_twitter = false
 
-let client: any;
+let client: any
 if (use_twitter) {
-  const Twitter = require("twitter");
+  const Twitter = require("twitter")
   client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_API_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_API_SECRET,
     access_token_key: process.env.TWITTER_ACCESS_TOKEN,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-  });
+  })
 }
 
 function home_timeline(): Promise<any> {
   if (use_twitter) {
-    return client.get("statuses/home_timeline.json", { count: 10 });
+    return client.get("statuses/home_timeline.json", { count: 10 })
   } else {
     return new Promise(resolve =>
-      resolve(
-        JSON.parse(fs.readFileSync("mock/statuses/home_timeline.json", "utf8"))
-      )
-    );
+      resolve(JSON.parse(fs.readFileSync("mock/statuses/home_timeline.json", "utf8")))
+    )
   }
 }
 
-const app = express();
+const app = express()
 
 app.get("/", (req, res) => {
-  res.send(JSON.stringify({ a: 0, b: 0 }));
-});
+  res.send(JSON.stringify({ a: 0, b: 0 }))
+})
 app.get("/a", (req, res) => {
-  res.send(JSON.stringify({ a: 1, b: 0 }));
-});
+  res.send(JSON.stringify({ a: 1, b: 0 }))
+})
 app.get("/b", (req, res) => {
-  res.send(JSON.stringify({ a: 0, b: 1 }));
-});
+  res.send(JSON.stringify({ a: 0, b: 1 }))
+})
 app.get("/c", (req, res) => {
-  res.send(JSON.stringify({ a: 1, b: 1 }));
-});
+  res.send(JSON.stringify({ a: 1, b: 1 }))
+})
 app.get("/timeline", (req, res) => {
   home_timeline().then((o: any) => {
-    res.send(JSON.stringify(o));
-  });
-});
+    res.send(JSON.stringify(o))
+  })
+})
 
-export const api = functions.https.onRequest(app);
+export const api_endpoint = functions.https.onRequest(express().use("/api", app))
